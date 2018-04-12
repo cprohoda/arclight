74bbf23372d93ab93e9bbf1ff753d94e4269a74e
Chris Prohoda <cgprohoda@gmail.com>
2018-04-11 23:42:49 -0400

    Make arclight compiler callable on file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-#![feature(generators)]
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs;
+use std::io::{Read, Write};
 
 mod Parser;
+mod ArclightSyntaxTree;
+mod Generator;
+mod Photon;
 
 fn main() {
-	let filename = "~/arclight/arclight_string_examples";
-	let mut f = File::open(filename).expect("File not found");
-	let mut contents = String::new();
-	f.read_to_string(&mut contents).expect("Failed to read the file");
+    let args: Vec<String> = env::args().collect();
+    let filename = args.get(1).unwrap().as_str();
+    let path = fs::canonicalize(filename).expect("");
+    let path_str = path.to_str().unwrap();
 
-    let blah = Parser::parse(&contents);
+    let mut f_in = fs::File::open(path_str).expect("Failed to find input file");
+    let mut contents = String::new();
+    f_in.read_to_string(&mut contents).expect("Failed to read input file contents");
+
+    let tokens = Parser::parse(&contents).unwrap();
+    let mut ast = ArclightSyntaxTree::ArclightSyntaxTree::new();
+    ast.build_at_marker(tokens);
+
+    let mut compiled = Generator::generate(ast).expect("Failed to generate arclight");
+
+    let mut f_out = fs::File::create(path_str.to_owned() + ".al").unwrap();
+    f_out.write(compiled.as_bytes());
+    f_out.flush();
 }
