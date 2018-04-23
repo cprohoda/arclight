098335aaa0fca1d405167b563596e6695d704302
Chris Prohoda <cgprohoda@gmail.com>
2018-04-22 21:54:55 -0400

    lib cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,6 @@
-#![feature(generators)]
-
-// pub mod lexer;
 pub mod Parser;
 pub mod Photon;
 pub mod Generator;
-// pub mod compiler;
-// pub mod repl;
-// pub mod branch;
 pub mod ArclightSyntaxTree;
-
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn it_works() {
-//     }
-// }
+pub mod ArclightObject;
+pub mod Preset;
